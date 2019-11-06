34c4a0fd0867079d1a8e10c6af6840fcc78827ff
Anjali <anjali.abndaru@sjsu.edu>
2019-11-06 13:09:23 -0800

    Adding modules in kafka frontend

---
+++ Kafka-frontend/config/settings.js
@@ -0,0 +1,14 @@
+'use strict';
+module.exports = {
+    'secret_key': 'secretshhh123',
+    'secret_key': 'secretshhh123',
+    'database_type': 'mongodb',
+    'database_username': 'root',
+    'database_password' : 'sjsu',
+    'database_host': 'localhost',
+    'database_port': '27017',
+    'database_name': 'grubhub',
+    'backend_port': '3001',
+    'connection_string': 'mongodb+srv://root:password631@273-qoppd.mongodb.net/canvas?retryWrites=true',
+    'client' : 'localhost'
+};
